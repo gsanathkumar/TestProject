32e16bcead6ee860afe7e1d80982698224b178a1
gsanathkumar <gsanathkumar@gmail.com>
2014-01-26 22:18:49 +0530

    test push

--- src/test/java/newfeatures/TestFileChannel.java
+++ src/test/java/newfeatures/TestFileChannel.java
@@ -1,17 +1,15 @@
 package test.java.newfeatures;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.nio.channels.FileChannel;
 
 public class TestFileChannel {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		FileInputStream fis = new FileInputStream("C:\\Users\\MANOJ\\Desktop\\TestFolder\\test.txt");
+		/*FileInputStream fis = new FileInputStream("C:\\Users\\MANOJ\\Desktop\\TestFolder\\test.txt");
 		FileOutputStream fos = new FileOutputStream("C:\\Users\\MANOJ\\Desktop\\TestFolder\\sample.txt");
 		FileChannel ifc = fis.getChannel();
-		FileChannel ofc = fos.getChannel();
+		FileChannel ofc = fos.getChannel();*/
 		System.out.println("test pull print");
+		System.out.println("test commit to test push");
 	}
 }
